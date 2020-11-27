457e1678e902b11c474d3dc65340ece753332f3a
ETHANATER50 <ethanater50@gmail.com>
2020-11-27 13:28:57 -0700

    character take 3

---
+++ TestProject/Player.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Character.h"
+
+namespace nc {
+	class NPC : public Character {
+	public:
+		NPC(int _maxHP, int _mDefense, int _mAttack, int _pDefense, int _pAttack, int _evasion, int _position, std::string _name, std::string _tag)
+			: Character(_maxHP, _mDefense, _mAttack, _pDefense, _pAttack, _evasion, _position, _name) {
+			tag = _tag;
+		}
+
+		void takeTurn();
+		void move();
+
+
+
+	protected:
+
+		std::string tag;
+	};
+}
