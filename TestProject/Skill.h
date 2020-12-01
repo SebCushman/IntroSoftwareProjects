47a20d5e9fe738e5bab97558b8a106e6ac4ff335
SebCushman <61478231+SebCushman@users.noreply.github.com>
2020-12-01 11:28:27 -0700

    Added Attack class and Skill Subclass

--- TestProject/Skill.h
+++ TestProject/Skill.h
@@ -2,16 +2,25 @@
 #include "pch.h"
 
 namespace nc {
-	class Skill {
+	class Attack {
 	public:
-
+		Attack(float accuracy, int minDam, int maxDam, bool isMagic) : m_accuracy{accuracy}, m_minDam { minDam }, m_maxDam{ maxDam }, m_isMagic{ isMagic }{}
 
 		friend class Character;
 
 	protected:
-		float accuracy{ 0 };
-		float minDam{ 0 };
-		float maxDam{ 0 };
-		bool isMagic{ false };
+		float m_accuracy{ 1 };
+		int m_minDam{ 1 };
+		int m_maxDam{ 3 };
+		bool m_isMagic{ false };
+	};
+
+	class Skill : public Attack {
+	public:
+		Skill(float accuracy, int minDam, int maxDam, bool isMagic, int maxCharges) : Attack(accuracy, minDam, maxDam, isMagic), m_maxCharges{ maxCharges }, m_currentCharges{maxCharges}{}
+
+	protected:
+		int m_currentCharges{ 0 };
+		int m_maxCharges{ 30 };
 	};
 }
