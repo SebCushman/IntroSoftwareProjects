c168338d052b1c37d46efdc6630dc6dc5897a99d
SebCushman <61478231+SebCushman@users.noreply.github.com>
2020-12-01 11:47:20 -0700

    Added Range to Attacks

--- TestProject/Skill.h
+++ TestProject/Skill.h
@@ -4,7 +4,7 @@
 namespace nc {
 	class Attack {
 	public:
-		Attack(float accuracy, int minDam, int maxDam, bool isMagic) : m_accuracy{accuracy}, m_minDam { minDam }, m_maxDam{ maxDam }, m_isMagic{ isMagic }{}
+		Attack(float accuracy, int minDam, int maxDam, int range, bool isMagic) : m_accuracy{ accuracy }, m_minDam{ minDam }, m_maxDam{ maxDam }, m_range{range}, m_isMagic{ isMagic }{}
 
 		friend class Character;
 
@@ -12,12 +12,13 @@ namespace nc {
 		float m_accuracy{ 1 };
 		int m_minDam{ 1 };
 		int m_maxDam{ 3 };
+		int m_range{ 1 };
 		bool m_isMagic{ false };
 	};
 
 	class Skill : public Attack {
 	public:
-		Skill(float accuracy, int minDam, int maxDam, bool isMagic, int maxCharges) : Attack(accuracy, minDam, maxDam, isMagic), m_maxCharges{ maxCharges }, m_currentCharges{maxCharges}{}
+		Skill(float accuracy, int minDam, int maxDam, int range, bool isMagic, int maxCharges) : Attack(accuracy, minDam, maxDam, range, isMagic), m_maxCharges{ maxCharges }, m_currentCharges{maxCharges}{}
 
 	protected:
 		int m_currentCharges{ 0 };
